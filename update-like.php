4891ba84d47ae4c533cc0aeb2f02d68e3d84b3df
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2023-10-11 13:12:15 +0545

    Update update-like.php

--- update-like.php
+++ update-like.php
@@ -11,6 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Update the like count in the database
     $updateQuery = "UPDATE comments SET `Like` = $newLikeCount WHERE id = $commentId";
     $updateResult = mysqli_query($conn, $updateQuery);
+    //make sure that the like will only update once, you can refer to the follow button in animedetails page on how to implement it.
 
     if ($updateResult) {
         echo "Success";
